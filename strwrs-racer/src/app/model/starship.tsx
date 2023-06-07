d81735ba59880625f8f65595293c16425e00e400
Humberto Grillo <humberto.celiasilva@gmail.com>
2023-06-06 23:14:25 -0300

    complete model

--- strwrs-racer/src/app/model/starship.tsx
+++ strwrs-racer/src/app/model/starship.tsx
@@ -1,11 +1,19 @@
 export default class Starship{
-    url!: string
-    name!: string
-    max_atmosphering_speed!: number
-    hyperdrive_rating!: number
-    MGLT!: number
+    url: string
+    name: string
+    model: string
+    starship_class: string
+    max_atmosphering_speed: number
+    hyperdrive_rating: number
+    MGLT: number // megalight per hour
 
-    constructor(){
-
+    constructor(obj:any){
+        this.url = obj.url
+        this.name = obj.name
+        this.model = obj.model
+        this.max_atmosphering_speed = obj.maxAtmospheringSpeed
+        this.hyperdrive_rating = obj.hyperdriveRating
+        this.MGLT = obj.MGLT
+        this.starship_class = obj.starship_class
     }
 }
--- strwrs-racer/src/app/model/vehicle.tsx
+++ strwrs-racer/src/app/model/vehicle.tsx
@@ -1,8 +1,16 @@
 export default class Vehicle{
-    url: string; // url works as id
-    name: string;
+    url: string // url works as id
+    name: string
+    model: string
+    manufacturer: string
+	max_atmosphering_speed: number
+	vehicle_class: string
     constructor(obj:any){
-        this.url = obj.url;
-        this.name = obj.name;
+        this.url = obj.url
+        this.name = obj.name
+        this.model = obj.model
+        this.manufacturer = obj.manufacturer
+        this.max_atmosphering_speed = obj.max_atmosphering_speed
+        this.vehicle_class = obj.vehicle_class
     }
 }
